084c8272197bd4746f7649333db23930e937550d
leo.lu <luqizheng@126.com>
2012-10-14 11:06:04 +0800

    add lost file.

---
+++ src/Extends/Daily/Qi.Attendance/DayOfWeekWorkTime.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Qi.Domain;
+
+namespace Qi.Attendance
+{
+    public class DayOfWeekWorkTime:DomainObject<DayOfWeekWorkTime,Guid>
+    {
+        private IList<Period> _workTimes;
+
+        public virtual IList<Period> WorkTimes
+        {
+            get
+            {
+                return _workTimes ?? (_workTimes = new List<Period>
+                    {
+                        new Period
+                            {
+                                Start = new Time(9, 0, 0),
+                                End = new Time(12, 0, 0)
+                            },
+                        new Period
+                            {
+                                Start = new Time(13, 0, 0),
+                                End = new Time(18, 0, 0)
+                            }
+                    });
+            }
+        }
+
+    }
+}
---
+++ src/Extends/Daily/Qi.Attendance/SpecialWorkDayMark.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using Qi.Domain;
+
+namespace Qi.Attendance
+{
+    public class SpecialWorkDayMark : DomainObject<SpecialWorkDayMark, string>
+    {
+        private IList<Period> _periods;
+
+        public SpecialWorkDayMark()
+        {
+            this.Id = "";
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public virtual DateTime Date { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public virtual IList<Period> Periods
+        {
+            get { return _periods ?? (_periods = new List<Period>()); }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="dateTime"></param>
+        /// <returns></returns>
+        public virtual bool IsMatch(DateTime dateTime)
+        {
+            foreach (Period perioid in Periods)
+            {
+                perioid.IsIn(dateTime);
+            }
+            throw new ApplicationException("fail to match");
+        }
+    }
+}
