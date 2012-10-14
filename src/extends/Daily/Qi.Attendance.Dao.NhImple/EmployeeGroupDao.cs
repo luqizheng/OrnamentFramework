23b32402bccd827eaf16150bd5cc11db426166c7
leo.lu <luqizheng@126.com>
2012-10-14 11:13:22 +0800

    Revert "add lost file."
    This reverts commit 1ff00f6bfca158a9d9d2603635068992e36b5d26.

--- src/extends/Daily/Qi.Attendance.Dao.NhImple/EmployeeGroupDao.cs
+++
@@ -1,8 +0,0 @@
-using System;
-using Qi.Domain.NHibernates;
-
-namespace Qi.Attendance.Dao.NhImple
-{
-    public class EmployeeGroupDao:DaoBase<Guid,EmployeeGroup>,IEmployeeGroupDao
-    {}
-}
--- src/extends/Daily/Qi.Attendance.Dao.NhImple/EquipmentDao.cs
+++
@@ -1,9 +0,0 @@
-using System;
-using Qi.Domain.NHibernates;
-
-namespace Qi.Attendance.Dao.NhImple
-{
-    //public class EquipmentDao :DaoBase<Guid, Equipment>, IEquipmentDao
-    //{
-    //}
-}
