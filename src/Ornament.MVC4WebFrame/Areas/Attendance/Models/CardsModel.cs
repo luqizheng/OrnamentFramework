7e6246e1fbfccb87536f538aede684896d562dfe
luqizheng <luqizheng@126.com>
2012-10-15 17:10:19 +0800

    add card manager.

---
+++ src/Ornament.MVC4WebFrame/Areas/Attendance/Models/CardsModel.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using Qi.Attendance;
+using Qi.Web.Mvc.Founders;
+
+namespace Ornament.MVCWebFrame.Areas.Attendance.Models
+{
+    public class CardsModel
+    {
+        public IList<Card> Cards { get; set; }
+
+        [IdFounder]
+        public virtual Employee Employee { get; set; }
+    }
+}
