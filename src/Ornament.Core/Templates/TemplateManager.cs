762424b787ed453a137ac73ded9bd9c0e9567af2
luqizheng@126.com <luqizheng@126.com>
2013-04-21 23:12:34 +0800

    add template manager.

---
+++ src/Ornament.Core/Templates/TemplateManager.cs
@@ -0,0 +1,42 @@
+using System;
+using System.IO;
+using System.Xml.Serialization;
+
+namespace Ornament.Templates
+{
+    public static class TemplateManager
+    {
+        public static T Get<T>(string path)
+        {
+            FileStream fs = null;
+            fs = new FileStream(path, FileMode.Open, FileAccess.Read);
+            try
+            {
+                var xs = new XmlSerializer(typeof (T));
+
+                return (T) xs.Deserialize(fs);
+            }
+            finally
+            {
+                fs.Close();
+            }
+        }
+
+        public static void Set<T>(string path, T tempalteObject) where T : class
+        {
+            if (tempalteObject == null)
+                throw new ArgumentNullException("tempalteObject", "Parameter tempalteObject is null!");
+
+            var fs = new FileStream(path, FileMode.Create, FileAccess.Write);
+            try
+            {
+                var xs = new XmlSerializer(typeof (T));
+                xs.Serialize(fs, tempalteObject);
+            }
+            finally
+            {
+                fs.Close();
+            }
+        }
+    }
+}
