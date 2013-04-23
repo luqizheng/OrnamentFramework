23de732b8390a97525ce0925c7c7b78041c31063
luqizheng@126.com <luqizheng@126.com>
2013-04-23 18:36:25 +0800

    add emmailTemplateManager

---
+++ src/Ornament.Core/Templates/EmailTempalgeManager.cs
@@ -0,0 +1,20 @@
+﻿using Qi;
+
+namespace Ornament.Templates
+{
+    public class EmailTempalgeManager
+    {
+        private const string _path = "~/Templates/Email/";
+
+        public EmailTemplate GetCreateUser()
+        {
+            return TemplateManager.Get<EmailTemplate>(ApplicationHelper.MapPath(_path + "Createuser.xml"));
+        }
+
+        public EmailTemplate ForgetPassword()
+        {
+            return TemplateManager.Get<EmailTemplate>(ApplicationHelper.MapPath(_path + "ForgetPassword.xml"));
+        }
+
+    }
+}
