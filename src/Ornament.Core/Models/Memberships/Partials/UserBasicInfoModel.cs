9ade0484a3a8e2c316d369cdddb33db7bdc09ec0
luqizheng@126.com <luqizheng@126.com>
2013-05-05 00:30:09 +0800

    add DisplayAttribute on Role and UserGroup.

--- src/Ornament.Core/Models/Memberships/Partials/UserBasicInfoModel.cs
+++ src/Ornament.Core/Models/Memberships/Partials/UserBasicInfoModel.cs
@@ -8,28 +8,28 @@ namespace Ornament.Models.Memberships.Partials
     public class UserBasicInfoModel
     {
         /// <summary>
-        /// 
         /// </summary>
-        [Display(Name = "LoginId", ResourceType = typeof(MembershipCommon))]
-        [Required(ErrorMessageResourceName = "error_MissLoginId", ErrorMessageResourceType = typeof(MemberShipModel))]
+        [Display(Name = "LoginId", ResourceType = typeof (MembershipCommon))]
+        [Required(ErrorMessageResourceName = "error_MissLoginId", ErrorMessageResourceType = typeof (MemberShipModel))]
         [RegularExpression(@"^[a-zA-z1-9_-]{1,20}", ErrorMessageResourceName = "LoginNotCorrectFormat",
-            ErrorMessageResourceType = typeof(ErrorMessage))]
+            ErrorMessageResourceType = typeof (ErrorMessage))]
         public string LoginId { get; set; }
 
         /// <summary>
-        /// 
         /// </summary>
-        [Display(Name = "Email", ResourceType = typeof(MembershipCommon))]
-        [Required(ErrorMessageResourceName = "error_missingEmailAddress", ErrorMessageResourceType = typeof(MemberShipModel))]
+        [Display(Name = "Email", ResourceType = typeof (MembershipCommon))]
+        [Required(ErrorMessageResourceName = "error_missingEmailAddress",
+            ErrorMessageResourceType = typeof (MemberShipModel))]
         [RegularExpression(@"\b[a-zA-Z0-9._%-]+@[a-zA-Z0-9.-]+\.[A-Za-z]{2,4}\b",
             ErrorMessageResourceName = "EmailNotRightFormat",
-            ErrorMessageResourceType = typeof(ErrorMessage))]
+            ErrorMessageResourceType = typeof (ErrorMessage))]
         [DataType(DataType.EmailAddress)]
         public string Email { get; set; }
 
-
+        [Display(Name = "Role", ResourceType = typeof (MembershipCommon))]
         public Role[] Roles { get; set; }
+
+        [Display(Name = "UserGroup", ResourceType = typeof (MembershipCommon))]
         public UserGroup[] UserGroups { get; set; }
-       
     }
 }
