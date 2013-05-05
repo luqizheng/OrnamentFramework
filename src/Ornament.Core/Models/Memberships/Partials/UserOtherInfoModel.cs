97be261a8efba6c36978f6a6fa724b9de097ca5b
luqizheng@126.com <luqizheng@126.com>
2013-05-05 15:39:55 +0800

    add other information view .

---
+++ src/Ornament.Core/Models/Memberships/Partials/UserOtherInfoModel.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ornament.MemberShip;
+
+namespace Ornament.Models.Memberships.Partials
+{
+    public class UserOtherInfoModel
+    {
+        public UserOtherInfoModel()
+        {
+
+        }
+
+        public UserOtherInfoModel(User user)
+        {
+            UpdateTime = user.UpdateTime;
+            this.CreateTime = user.CreateTime;
+            this.LastPasswordChangedDate = user.LastPasswordChangedDate;
+            this.LastLockoutDate = user.LastLockoutDate;
+            this.LastLoginDate = user.LastLoginDate;
+            this.LastActivityDate = user.LastActivityDate;
+        }
+        public DateTime? UpdateTime { get; set; }
+
+        public DateTime CreateTime { get; set; }
+
+        public DateTime? LastPasswordChangedDate { get; set; }
+
+        public DateTime? LastLockoutDate { get; set; }
+
+        public DateTime? LastLoginDate { get; set; }
+
+        public DateTime? LastActivityDate { get; set; }
+    }
+}
