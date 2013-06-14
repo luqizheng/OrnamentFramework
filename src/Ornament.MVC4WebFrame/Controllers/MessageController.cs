feef63bcc8e7cb2212bfef346286a304239beec7
leo <lqz.leo@gmail.com>
2013-06-14 11:46:30 +0800

    add Task API Controller and page.

--- src/Ornament.MVC4WebFrame/Controllers/MessageController.cs
+++ src/Ornament.MVC4WebFrame/Controllers/MessageController.cs
@@ -3,17 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Ornament.Messages.Dao;
 
 namespace Ornament.MVCWebFrame.Controllers
 {
     [Authorize]
     public class MessageController : Controller
     {
+        private readonly IMessageDaoFactory _messageDaoFactory;
+
+        public MessageController(IMessageDaoFactory messageDaoFactory)
+        {
+            _messageDaoFactory = messageDaoFactory;
+        }
+
         //
         // GET: /Message/
 
         public ActionResult Index()
         {
+
             return View();
         }
 
