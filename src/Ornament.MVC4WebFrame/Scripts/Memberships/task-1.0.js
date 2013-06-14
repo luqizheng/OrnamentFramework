d57946e3137b28a74cb527ff12821e0d7c5d8335
leo <lqz.leo@gmail.com>
2013-06-14 11:46:10 +0800

    add bootstrap less.

---
+++ src/Ornament.MVC4WebFrame/Scripts/Memberships/task-1.0.js
@@ -0,0 +1,19 @@
+﻿//jquery.js
+define(function (require, exports, module) {
+
+    var myTask = {
+        pageIndex: 0,
+        pageSize: 10
+    };
+    var innerFunction = {
+        myTask: function (opts, func) {
+
+            $.post("/Api/Tasks/MyTask", $.extends({}, myTask, opts), func);
+
+
+        }
+
+    };
+    module.exports = innerFunction;
+
+});
