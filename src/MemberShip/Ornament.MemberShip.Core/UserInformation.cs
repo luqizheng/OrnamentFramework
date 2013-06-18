0407a0c9e1c1ba90ad3a2a1dcffc255484e8d64d
leo <lqz.leo@gmail.com>
2013-06-18 18:08:07 +0800

    emial could be empty. and set length to 64

--- src/MemberShip/Ornament.MemberShip.Core/UserInformation.cs
+++ src/MemberShip/Ornament.MemberShip.Core/UserInformation.cs
@@ -1,7 +1,5 @@
 using System;
 using System.ComponentModel.DataAnnotations;
-using System.Security.AccessControl;
-using System.Text.RegularExpressions;
 using Ornament.MemberShip.Languages;
 
 namespace Ornament.MemberShip
@@ -35,43 +33,46 @@ namespace Ornament.MemberShip
         //private User User { get; set; }
 
         /// <summary>
-        /// Gets or sets Phone.
+        ///     Gets or sets Phone.
         /// </summary>
         /// <value>
-        /// The phone.
+        ///     The phone.
         /// </value>
-        [Display(Name = "Phone", ResourceType = typeof(MembershipCommon)),
+        [Display(Name = "Phone", ResourceType = typeof (MembershipCommon)),
          StringLength(30, MinimumLength = 0, ErrorMessage = "长度必须在1至30之间")]
         public virtual string Phone
         {
             get { return _phone; }
             set
             {
-                if (this.Id != string.Empty)
+                if (Id != string.Empty)
                 {
-                    this.ModifyUpdateTime();
+                    ModifyUpdateTime();
                 }
                 _phone = value;
             }
         }
 
         /// <summary>
-        /// Gets or sets Email.
+        ///     Gets or sets Email.
         /// </summary>
         /// <value>
-        /// The email.
+        ///     The email.
         /// </value>
-        [DataType(DataType.EmailAddress, ErrorMessageResourceName = "EmailNotRightFormat",ErrorMessageResourceType = typeof(ErrorMessage)),
-         Display(Name = "Email", ResourceType = typeof(MembershipCommon)),
-        RegularExpressionAttribute(@"\b[a-zA-Z0-9._%-]+@[a-zA-Z0-9.-]+\.[A-Za-z]{2,4}\b", ErrorMessageResourceName = "EmailNotRightFormat", ErrorMessageResourceType = typeof(ErrorMessage))]
+        [DataType(DataType.EmailAddress, ErrorMessageResourceName = "EmailNotRightFormat",
+            ErrorMessageResourceType = typeof (ErrorMessage)),
+         Display(Name = "Email", ResourceType = typeof (MembershipCommon)),
+         RegularExpression(@"\b[a-zA-Z0-9._%-]+@[a-zA-Z0-9.-]+\.[A-Za-z]{2,4}\b",
+             ErrorMessageResourceName = "EmailNotRightFormat", ErrorMessageResourceType = typeof (ErrorMessage))]
+        [MaxLength(64)]
         public virtual string Email
         {
             get { return _email; }
             set
             {
-                if (!String.IsNullOrEmpty(this.Id))
+                if (!String.IsNullOrEmpty(Id))
                 {
-                    this.ModifyUpdateTime();
+                    ModifyUpdateTime();
                 }
                 //const string pattern = @"\b[a-zA-Z0-9._%-]+@[a-zA-Z0-9.-]+\.[A-Za-z]{2,4}\b";
                 //if (!Regex.IsMatch(value, pattern))
@@ -79,7 +80,9 @@ namespace Ornament.MemberShip
 
                 _email = value;
             }
+            
         }
+
         //}
 
         #endregion
--- src/Ornament.Core/Messages/OrnamentMessageManager.cs
+++ src/Ornament.Core/Messages/OrnamentMessageManager.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using Ornament.MemberShip;
 using Ornament.MemberShip.Dao;
 using Ornament.Messages.Dao;
 
@@ -7,22 +6,14 @@ namespace Ornament.Messages
 {
     public class OrnamentMessageManager
     {
-        
-
-
         public OrnamentMessageManager(IMessageDaoFactory messageFacotry, IMemberShipFactory memberShipFactory)
         {
             if (messageFacotry == null) throw new ArgumentNullException("messageFacotry");
             InfoDaoFactory = messageFacotry;
             MemberShipFactory = memberShipFactory;
-            
         }
 
         public IMessageDaoFactory InfoDaoFactory { get; set; }
         public IMemberShipFactory MemberShipFactory { get; private set; }
-
-     
-      
-      
     }
 }
