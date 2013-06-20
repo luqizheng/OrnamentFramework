fd518286b1cdf5c51405a9c40077379b37a7de4e
leo <lqz.leo@gmail.com>
2013-06-20 12:01:29 +0200

    add DateTime extender for js

---
+++ src/Ornament.MVC4WebFrame/Scripts/util/DateTime.js
@@ -0,0 +1,17 @@
+﻿var getUtcHourOffset = function () {
+    return new Date().getTimezoneOffset() / 60 * -1;
+};
+
+function correctTimeZone(bLocation) {
+    if (document.location.href.indexOf("utc=" + getUtcHourOffset()) == -1) {
+        console.log(getUtcHourOffset);
+        var url = (document.location.href.indexOf("?") != -1 ? "&" : "?") + "utc=" + getUtcHourOffset();
+        if (bLocation) {
+            console.log(document.location + url);
+            document.location = document.location + url;
+        } else {
+            console.log("<script src='{0}' type='text/javascript'></script>".replace("{0}", "/api/Client/CorrectTime?" + url.substr(1)));
+            $("body").append("<script src='{0}' type='text/javascript'></script>".replace("{0}", "/api/Client/CorrectTime?"+url.substr(1)));
+        }
+    }
+}
