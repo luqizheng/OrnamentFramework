73802a3262604aafe89c4db53b4d43d761254eaf
leo <lqz.leo@gmail.com>
2013-06-20 11:46:24 +0200

    add Client api controller.

---
+++ src/Ornament.MVC4WebFrame/Controllers/ClientController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+using Ornament.Web;
+using Ornament.Web.HttpModel;
+
+namespace Ornament.MVCWebFrame.Controllers
+{
+    public class ClientController : ApiController
+    {
+         [System.Web.Http.HttpGet]
+        public void CorrectTimeZone(int? utc)
+        {
+             if (utc != null)
+             {
+                 var t =
+                     OrnamentContext.MemberShip.CorrectClientUtcTime(utc.Value);
+                 OrnamentModule.SetClientOffsetHour(t);
+             }
+
+        }
+
+    }
+}
