6602d55610d37bf4a20163cd5e9d41f6fdc62283
leo <luqizheng@126.com>
2013-07-02 16:26:36 +0800

    site infor.

---
+++ src/Ornament.MVC4WebFrame/Areas/Settings/Controllers/SiteController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Ornament.MVCWebFrame.Areas.Settings.Controllers
+{
+    public class SiteController : Controller
+    {
+        //
+        // GET: /Settings/SiteInfo/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
