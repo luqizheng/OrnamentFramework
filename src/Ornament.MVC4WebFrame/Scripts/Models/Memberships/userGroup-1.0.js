99b08b7eaa8916c644b409da09fde75527a92e05
luqizheng@126.com <luqizheng@126.com>
2013-07-02 20:40:31 +0800

    improve js size.

--- src/Ornament.MVC4WebFrame/Scripts/Models/Memberships/userGroup-1.0.js
+++ src/Ornament.MVC4WebFrame/Scripts/Models/Memberships/userGroup-1.0.js
@@ -5,7 +5,7 @@ define(function (require) {
     var api = require("/models/util/select2Helper.js");
     return {
         select2: function (selector, initData) {
-            return api.select2(selector, { url: "/api/usergroups/Match" }, initData);
+            return api.select2(selector, { url: "/api/usergroups" }, initData);
         }
     };
 });
