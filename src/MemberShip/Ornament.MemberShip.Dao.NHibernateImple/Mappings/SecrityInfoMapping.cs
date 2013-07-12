c623facdcaeb4d630b62238dd99818c43fd9131d
leo <luqizheng@126.com>
2013-07-12 16:40:15 +0800

    Do Not use lazyload in

--- src/MemberShip/Ornament.MemberShip.Dao.NHibernateImple/Mappings/SecrityInfoMapping.cs
+++ src/MemberShip/Ornament.MemberShip.Dao.NHibernateImple/Mappings/SecrityInfoMapping.cs
@@ -12,9 +12,9 @@ namespace Ornament.MemberShip.Dao.NHibernateImple.Mappings
             Map(s => s.Password)
                 .Access.ReadOnlyPropertyThroughCamelCaseField(Prefix.Underscore).Length(64); ;
 
-            Map(s => s.PasswordAnswer).Length(100).LazyLoad()
+            Map(s => s.PasswordAnswer).Length(100)
                                       .Access.ReadOnlyPropertyThroughCamelCaseField(Prefix.Underscore); ;
-            Map(s => s.PasswordQuestion).Length(100).LazyLoad()
+            Map(s => s.PasswordQuestion).Length(100)
                                         .Access.ReadOnlyPropertyThroughCamelCaseField(Prefix.Underscore);
             References(s => s.User);
             Map(s => s.LastLoginDate);
