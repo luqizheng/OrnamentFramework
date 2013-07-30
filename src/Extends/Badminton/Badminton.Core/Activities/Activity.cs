f33facaa96fd725db64ceba0e8f9db47369ecf72
luqizheng@126.com <luqizheng@126.com>
2013-07-30 23:13:02 +0800

    add Activity Mapping.

--- src/Extends/Badminton/Badminton.Core/Activities/Activity.cs
+++ src/Extends/Badminton/Badminton.Core/Activities/Activity.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
 using Iesi.Collections.Generic;
 using Qi.Domain;
 
@@ -10,8 +9,8 @@ namespace Badminton.Activities
     /// </summary>
     public class Activity : DomainObject<Activity, string>
     {
-        private IList<ConsumablesHistory> _consumablesHistories;
-        private Iesi.Collections.Generic.ISet<MemberJoinStatus> _joinMembers;
+        private ISet<ConsumablesHistory> _consumablesHistories;
+        private ISet<MemberJoinStatus> _joinMembers;
 
         /// <summary>
         ///     活动开始时间
@@ -34,15 +33,15 @@ namespace Badminton.Activities
         /// <summary>
         ///     消耗品历史记录
         /// </summary>
-        public virtual IList<ConsumablesHistory> ConsumablesHistories
+        public virtual ISet<ConsumablesHistory> ConsumablesHistories
         {
-            get { return _consumablesHistories ?? (_consumablesHistories = new List<ConsumablesHistory>()); }
+            get { return _consumablesHistories ?? (_consumablesHistories = new HashedSet<ConsumablesHistory>()); }
         }
 
         /// <summary>
         ///     活动参加人数
         /// </summary>
-        public virtual Iesi.Collections.Generic.ISet<MemberJoinStatus> JoinMember
+        public virtual ISet<MemberJoinStatus> JoinMembers
         {
             get { return (_joinMembers) ?? (_joinMembers = new HashedSet<MemberJoinStatus>()); }
         }
