7785a1de3f52ab987dc35390a5a27c0095b0b65c
leo <luqizheng@126.com>
2013-08-01 12:02:20 +0800

    add templ for

--- src/Ornament.MVC4WebFrame/Scripts/Models/Messages/pm-1.0.js
+++ src/Ornament.MVC4WebFrame/Scripts/Models/Messages/pm-1.0.js
@@ -6,33 +6,62 @@ PM dialog UI 1.0
 */
 define(function (require) {
     require('/bundles/bootstrap.js');
-    var api = require('/models/personal.js');
-    var tipsMsge = require('/models/personal.js');
-    var $this = $("#pmEditor").modal({
+    var api = require('/models/personal.js'),
+     tipsMsge = require('/models/personal.js'),
+        ownerName = "",
+        lastTime = null,
+    $this = $("#pmEditor").modal({
         show: false,
         hidden: function () {
             $("textarea", $this).val("");
-            tipsMsge.watch();
+            tipsMsge.watch(); //重新开启top menu的监控
         },
-        shown:function() {
-            tipsMsge.unWatch();
+        shown: function () {
+            tipsMsge.unWatch(); //stop topmenu 的message检查
         }
-        
-    }),
-       ownerName = "";
+    }),buildTemp = function (chatItem) {
+            return {
+                dt: $("<dt><h6>" + chatItem.publisher + " <small>" + chatItem.createTime + "</small></h6></dt>"),
+                dd: $("<dd><div>" + chatItem.content + "</div></dd>")
+            };
+    };
     
+    function getChat(userId, userName) {
+        ownerName = userName;
+        $("input", $this).val(userId);
+        api.getChat(userId, 0,lastTime, function (d) {
+            var chat = [];
+            lastTime = d[d.length - 1].createTime;
+            $(d).each(function () {
+                var isOwnerMgs = ownerName == this.publisher, item = buildTemp(this);
+                if (!isOwnerMgs) {
+                    $([item.dt, item.dd]).each(function () {
+                        $(this).addClass("clearfix").find(":first").addClass("pull-right");
+                    });
+                }
+                chat.push(item.dt);
+                chat.push(item.dd);
+            });
+            var $chat = $(".chat", $this);
+            if (chat.length != 0) {
+                $chat.html("").append(chat).scrollTop($chat[0].scrollHeight).closest(".body").show();
+            } else {
+                $chat.closest(".widget").hide();
+            }
+        });
+        $this.modal('show');
+    }
     $("a:eq(1)", $this).click(function (e) {
         e.preventDefault();
         e.stopPropagation();
-
         var content = $("textarea", $this).val(), receiver = $("input", $this).val();
         api.sendPm(content, receiver,
             function (d) {
                 if (d) {
-                    var dt = "<dt>" + ownerName + "</dt>",$chat=$(".chat", $this);
-                    dt += "<dd>" + content + "</dd>";
-                    $chat.append(dt).scrollTop($chat.scrollHeight);//.closest(".body").show();
-                    $("textarea").val("");
+                    var item = buildTemp(d), $chat = $(".chat", $this);
+                    $chat.append(item.dt).append(item.dd);
+                    $chat.scrollTop($chat.scrollHeight).closest(".widget").show();
+                    $("textarea", $this).val("");
                 } else {
                     alert("send failed");
                 }
@@ -40,33 +69,8 @@ define(function (require) {
     });
 
     return {
-        show: function (userId, userName) {
-            ownerName = userName;
-            $("input", $this).val(userId);
-            api.getChat(userId, 0, function (d) {
-                var chat = [];
-                $(d).each(function () {
-                    var chatItem = this;
-                    var isOwnerMgs = ownerName == chatItem.publisher;
-                    var dt = $("<dt><h6>" + chatItem.publisher + " <small>" + chatItem.createTime + "</small></h6></dt>");
-                    var dd = $("<dd><div>" + chatItem.content + "</div></dd>");
-                    if (!isOwnerMgs) {
-                        $([dt, dd]).each(function () {
-                            $(this).addClass("clearfix").find(":first").addClass("pull-right");
-                        });
-                        //dd.addClass("clearfix").find(":first").addClass("pull-right");
-                    }
-                    chat.push(dt);
-                    chat.push(dd);
-                });
-                var $chat = $(".chat", $this);
-                if (chat.length != 0) {
-                    $chat.html("").append(chat).scrollTop($chat[0].scrollHeight).closest(".body").show();
-                } else {
-                    $chat.closest(".body").hide();
-                }
-            });
-            $this.modal('show');
+        show: function (relativeUserId, myName) {
+            getChat(relativeUserId, myName);
         }
     };
 });
