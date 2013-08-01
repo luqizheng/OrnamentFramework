1cc94945549ec0a27f75e4b10b6267844b698701
luqizheng@126.com <luqizheng@126.com>
2013-08-02 00:46:20 +0800

    aaa

--- src/Ornament.MVC4WebFrame/Controllers/MyMessageController.cs
+++ src/Ornament.MVC4WebFrame/Controllers/MyMessageController.cs
@@ -1,6 +1,8 @@
 ﻿using System.Web.Mvc;
+using Ornament.Messages;
 using Ornament.Messages.Dao;
 using Ornament.Web;
+using Qi.Web.Mvc;
 
 namespace Ornament.MVCWebFrame.Controllers
 {
@@ -17,19 +19,26 @@ namespace Ornament.MVCWebFrame.Controllers
 
         //
         // GET: /Message/
-
+        [Session]
         public ActionResult Index()
         {
 
             ViewData["notify"] =
                 _messageDaoFactory.PersonalMessageDao.GetLastMessageForEachUser(OrnamentContext.MemberShip.CurrentUser(), 0, 40);
 
-            
+
             ViewData["annoumcement"] = _messageDaoFactory.AnnouncementDao.GetByUser(OrnamentContext.MemberShip.CurrentUser(), 0, 20);
 
             return View();
         }
 
+        [Session]
+        public ActionResult MakeRead(string id)
+        {
+            var a = _messageDaoFactory.NotifyMessageDao.Get(id);
+            a.ReadStatus = ReadStatus.Read;
+            return Json(true);
+        }
 
     }
 }
