37c29bacc05d7c16149abe12f342907d0d3e1dca
leo <luqizheng@126.com>
2013-08-06 10:09:14 +0800

    add Yard/Create.cshtml

---
+++ src/Ornament.MVC4WebFrame/Areas/Badminton/Controllers/YardController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Ornament.MVCWebFrame.Areas.Badminton.Controllers
+{
+    public class YardController : Controller
+    {
+        //
+        // GET: /Badminton/Yard/
+
+        public ActionResult Create(int? gymasiumId)
+        {
+            return View();
+        }
+
+    }
+}
