23952cc529e64190109d32e37e83eca22b18358d
leo <luqizheng@126.com>
2013-08-15 23:14:32 +0800

    bug fix for account index js

---
+++ src/Ornament.MVC4WebFrame/Scripts/Modules/Views/Accounts/Index.js
@@ -0,0 +1,10 @@
+﻿/// <reference path="../_appLayout.js" />
+define(function (require) {
+    require("../_applayout.js");
+    var $ = require("jquery");
+    $('a[data-toggle="tab"]').on('shown', function (e) {
+        $($(this).attr("href")).find("input:first").focus();
+    });
+
+});
+
