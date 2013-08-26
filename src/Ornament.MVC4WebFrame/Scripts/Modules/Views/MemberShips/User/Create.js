8d4352fd3231ebadaac1bd72c76706173f84552d
leo <luqizheng@126.com>
2013-08-26 17:28:59 +0800

    add use and create js

---
+++ src/Ornament.MVC4WebFrame/Scripts/Modules/Views/MemberShips/User/Create.js
@@ -0,0 +1,7 @@
+﻿/// <reference path="../../_appLayout.js" />
+define(function (require) {
+    require("../../_appLayout.js");
+    var $ = require("jquery");
+    require("validate")($);
+    $("form").bootstrapMakeUp();
+});
