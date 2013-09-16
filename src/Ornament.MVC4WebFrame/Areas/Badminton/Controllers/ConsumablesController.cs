4aebd39574b0a539770f26892e27bee3fc7e687f
leo <luqizheng@126.com>
2013-09-16 16:25:40 +0800

    edit

---
+++ src/Ornament.MVC4WebFrame/Areas/Badminton/Controllers/ConsumablesController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Ornament.MVCWebFrame.Areas.Badminton.Controllers
+{
+    public class ConsumablesController : Controller
+    {
+        //
+        // GET: /Badminton/Consumables/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
