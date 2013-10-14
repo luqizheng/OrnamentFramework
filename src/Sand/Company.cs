f655a16cf0077f653fa8550a4fb3806d3b39ff30
leo <luqizheng@126.com>
2013-10-14 15:16:22 +0800

    add c

---
+++ src/Sand/Company.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using Qi.Domain;
+
+namespace Sand
+{
+    public class ContactItem
+    {
+        /// <summary>
+        ///     联络内容
+        /// </summary>
+        public string Content { get; set; }
+
+        /// <summary>
+        ///     联络名称
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        ///     联络人
+        /// </summary>
+        public string Contact { get; set; }
+    }
+
+    public class Company : DomainObject<Company, string>
+    {
+
+        public virtual string Name { get; set; }
+        IList<ContactItem> _contacts = new List<ContactItem>();
+        public virtual IList<ContactItem> Contacts { get { return _contacts ?? (_contacts = new List<ContactItem>()); } }
+    }
+
+    public class Shop : DomainObject<Shop, string>
+    {
+        public virtual string Name { get; set; }
+        IList<ContactItem> _contacts = new List<ContactItem>();
+        public virtual IList<ContactItem> Contacts { get { return _contacts ?? (_contacts = new List<ContactItem>()); } }
+    }
+
+    public class Client : DomainObject<Client, string>
+    {
+        public virtual string Name { get; set; }
+        
+    }
+}
