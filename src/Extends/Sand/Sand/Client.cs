703b768c4d6e0ecbed88cb7ee5bc93aa5cb191a1
leo <luqizheng@126.com>
2013-10-14 17:41:11 +0800

    move Sand project to Sand extender.

---
+++ src/Extends/Sand/Sand/Client.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Qi.Domain;
+
+namespace Sand
+{
+    public class Client : DomainObject<Client, string>
+    {
+        protected Client()
+        {
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="model"></param>
+        /// <param name="code"></param>
+        public Client(ClientModel model, string code)
+        {
+            if (model == null) throw new ArgumentNullException("model");
+            if (!String.IsNullOrEmpty(code))
+                throw new ArgumentNullException("code");
+            Model = model;
+            Code = code;
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        public virtual ClientModel Model { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public virtual string Code { get; set; }
+    }
+}
