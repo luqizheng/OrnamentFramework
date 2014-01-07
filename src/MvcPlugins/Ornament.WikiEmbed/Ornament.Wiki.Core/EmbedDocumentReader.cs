f71a4dea69cd87ecd3d3118cf360afedcd8a7503
leo <luqizheng@126.com>
2014-01-07 15:07:26 +0800

    update packages.config

---
+++ src/MvcPlugins/Ornament.WikiEmbed/Ornament.Wiki.Core/EmbedDocumentReader.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using WikiPlex;
+
+namespace Ornament.Wiki.Core
+{
+    public class EmbedDocumentReader
+    {
+        public static string Reader(string path, Assembly assembly)
+        {
+            Stream stream = assembly.GetManifestResourceStream(path);
+            if (stream == null)
+            {
+                throw new Exception();
+            }
+            var engine = new WikiEngine();
+            using (var text = new StreamReader(stream))
+            {
+                return engine.Render(text.ReadToEnd());
+            }
+        }
+    }
+}
