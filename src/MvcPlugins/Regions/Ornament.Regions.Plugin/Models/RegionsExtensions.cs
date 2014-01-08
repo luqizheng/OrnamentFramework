948af4b7f1d79d0df6e464512e9f41db209a7caf
leo <luqizheng@126.com>
2014-01-08 13:13:10 +0800

    add exteions for membership and regions.

---
+++ src/MvcPlugins/Regions/Ornament.Regions.Plugin/Models/RegionsExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq.Expressions;
+using System.Web.Mvc;
+using System.Web.Mvc.Html;
+
+// ReSharper disable once CheckNamespace
+
+namespace Ornament.Web
+{
+    public static class RegionsExtensions
+    {
+        public static MvcHtmlString EditorForArea<TModel, TValue>(this HtmlHelper<TModel> helper,
+            Expression<Func<TModel, TValue>> func)
+        {
+            return helper.EditorFor(func, "~/protableAreas/Regions/Area");
+        }
+    }
+}
---
+++ src/Ornament.MemberShip.Plugin/Models/MembershipsExtensions.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Linq.Expressions;
+using System.Web.Mvc;
+
+// ReSharper disable once CheckNamespace
+
+namespace Ornament.Web
+{
+    public static class MembershipsExtensions
+    {
+        private const string MemberShips = "Memberships";
+
+        public static MvcHtmlString EditorForUsers<TModel, TValue>(this HtmlHelper<TModel> helper,
+            Expression<Func<TModel, TValue>> func)
+        {
+            return helper.EditorFor(func, "User[]");
+        }
+
+        public static MvcHtmlString EditorForRoles<TModel, TValue>(this HtmlHelper<TModel> helper,
+            Expression<Func<TModel, TValue>> func)
+        {
+            return helper.EditorFor(func, "~/protableAreas/" + MemberShips + "/Role[]");
+        }
+
+        public static MvcHtmlString EditorForUserGroups<TModel, TValue>(this HtmlHelper<TModel> helper,
+            Expression<Func<TModel, TValue>> func)
+        {
+            return helper.EditorFor(func, "~/protableAreas/" + MemberShips + "/UserGroup[]");
+        }
+
+
+        public static MvcHtmlString EditorForOrg<TModel, TValue>(this HtmlHelper<TModel> helper,
+            Expression<Func<TModel, TValue>> func)
+        {
+            return helper.EditorFor(func, "~/protableAreas/" + MemberShips + "/Org");
+        }
+
+        private static MvcHtmlString EditorFor<TModel, TValue>(this HtmlHelper<TModel> helper,
+            Expression<Func<TModel, TValue>> func, string template)
+        {
+            return helper.EditorFor(func, string.Format("~/protableAreas/{0}/{1}", MemberShips, template));
+        }
+    }
+}
