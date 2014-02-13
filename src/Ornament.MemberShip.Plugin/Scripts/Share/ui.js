54a46cf35a1246c7d5d0492d9d5efd5a1d1f37ca
leo <luqizheng@126.com>
2014-02-13 11:56:46 +0800

    修复错误，现在多选select2空间，全部用复数，单选用单数，如 ui.select2.User ,ui.select2.Users

--- src/Ornament.MemberShip.Plugin/Scripts/Share/ui.js
+++ src/Ornament.MemberShip.Plugin/Scripts/Share/ui.js
@@ -9,8 +9,19 @@
         minimumInputLength: 1,
         multiple: true,
         placeholder: "Please input a char"
-    };
-    var userOptions = {
+    },
+    orgOpts = {
+        ajax:
+            { url: "/api/Orgs" },
+        multiple: false
+    },
+    ugOpts = {
+        ajax: { url: "/api/usergroups" }
+    },
+    roleOpts = {
+        ajax: { url: "/api/Roles" }
+    },
+    userOpts = {
         ajax: {
             data: function (term, page) { // page is the one-based page number tracked by Select2
                 return {
@@ -32,24 +43,24 @@
                 return { results: r, more: more };
             }
         }
-    };
-    var ajaxOpts = {
-        data: function (term, page) { // page is the one-based page number tracked by Select2
-            return {
-                name: term + "%",
-                page: (page - 1), // page number
-            };
-        },
-        results: function (data, page) {
-            var more = (page * 10) < data.total; // whether or not there are more results available
-            // notice we return the value of more so Select2 knows if more results can be loaded
-            var r = [];
-            $(data).each(function () {
-                r.push({ id: this.id, text: this.Name });
-            });
-            return { results: r, more: more };
-        }
-    };
+    },
+     ajaxOpts = {
+         data: function (term, page) { // page is the one-based page number tracked by Select2
+             return {
+                 name: term + "%",
+                 page: (page - 1), // page number
+             };
+         },
+         results: function (data, page) {
+             var more = (page * 10) < data.total; // whether or not there are more results available
+             // notice we return the value of more so Select2 knows if more results can be loaded
+             var r = [];
+             $(data).each(function () {
+                 r.push({ id: this.id, text: this.Name });
+             });
+             return { results: r, more: more };
+         }
+     };
 
     var forData = function (selector, opts, data) {
         var $tag = $(selector),
@@ -83,29 +94,22 @@
 
     return {
         select2: {
-            org:
-                function (selector, initData) {
-                    var opts = {
-                        ajax:
-                            { url: "/api/Orgs" },
-                        multiple: false
-                    };
-                    return forData(selector, opts, initData);
-                },
-            role: function (selector, initData) {
-                return forData(selector, { ajax: { url: "/api/Roles" } }, initData);
+            org: function(selector, initData) {
+                 return forData(selector, orgOpts, initData);
+            },
+            roles: function (selector, initData) {
+                return forData(selector, roleOpts, initData);
             },
             user: function (selector, initData) {
-                userOptions.maximumSelectionSize = 1;
-                return forData(selector, userOptions, initData);
+                userOpts.maximumSelectionSize = 1;
+                return forData(selector, userOpts, initData);
             },
             users: function (selector, initData) {
-                return forData(selector, userOptions, initData);
+                return forData(selector, userOpts, initData);
             },
-            userGroup: function (selector, initData) {
-                return forData(selector, { ajax: { url: "/api/usergroups" } }, initData);
+            userGroups: function (selector, initData) {
+                return forData(selector, ugOpts, initData);
             }
-
         }
     };
 })
