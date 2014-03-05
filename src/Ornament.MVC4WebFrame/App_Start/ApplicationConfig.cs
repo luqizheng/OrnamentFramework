f4403c7a34f19906241bac28361678fa0fc6cfd0
leo <luqizheng@126.com>
2014-03-06 00:44:58 +0800

    修改语言显示的方案.

--- src/Ornament.MVC4WebFrame/App_Start/ApplicationConfig.cs
+++ src/Ornament.MVC4WebFrame/App_Start/ApplicationConfig.cs
@@ -8,11 +8,11 @@ namespace Ornament.MVCWebFrame
         public static void Register(OrnamentConfiguration context)
         {
             context.Languages.Add(new Language("English", "en") { IsDefault = true });
-            context.Languages.Add(new Language("中文繁體", "zh-HK")
+            context.Languages.Add(new Language("中文(繁體)", "zh-HK")
             {
                 MatchKey = new[] { "zh-TW", "zh-MO", "zh-HK", "zh" }
             });
-            context.Languages.Add(new Language("中文简体", "zh-CN")
+            context.Languages.Add(new Language("中文(简体)", "zh-CN")
             {
                 MatchKey = new[] { "zh-CN", "zh" }
             });
