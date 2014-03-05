e0c9270ad0afc618c0e08e8bd6c06b7f6dee17f8
leo <luqizheng@126.com>
2014-03-05 17:36:27 +0800

    修复多语言

--- src/Ornament.MVC4WebFrame/App_Start/ApplicationConfig.cs
+++ src/Ornament.MVC4WebFrame/App_Start/ApplicationConfig.cs
@@ -8,11 +8,11 @@ namespace Ornament.MVCWebFrame
         public static void Register(OrnamentConfiguration context)
         {
             context.Languages.Add(new Language("English", "en") { IsDefault = true });
-            context.Languages.Add(new Language("中文繁體", "zh-Hant")
+            context.Languages.Add(new Language("中文繁體", "zh-HK")
             {
                 MatchKey = new[] { "zh-TW", "zh-MO", "zh-HK", "zh" }
             });
-            context.Languages.Add(new Language("中文简体", "zh-Hans")
+            context.Languages.Add(new Language("中文简体", "zh-CN")
             {
                 MatchKey = new[] { "zh-CN", "zh" }
             });
