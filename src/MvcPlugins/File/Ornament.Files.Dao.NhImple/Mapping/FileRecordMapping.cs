064c5f620d4aade7c7d60c30220d90e664916be4
leo <luqizheng@126.com>
2014-03-21 16:35:34 +0800

    增加一个File项目区管理文件。

---
+++ src/MvcPlugins/File/Ornament.Files.Dao.NhImple/Mapping/FileRecordMapping.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentNHibernate.Mapping;
+
+namespace Ornament.Files.Dao.Mapping
+{
+    public class FileRecordMapping:ClassMap<FileRecord>
+    {
+        public FileRecordMapping()
+        {
+            this.Table("Files_FileRecord");
+            this.Id(s => s.Id).GeneratedBy.UuidHex("N").Length(32);
+            this.Map(s => s.Name);
+            this.Map(s => s.FullPath);
+            this.Map(s => s.CreateTime).Unique().Index("idx_createTime").Not.Update();
+            this.Map(s => s.SignCode).Unique().Index("idx_signCode").Not.Update();
+
+            this.References(s => s.Creator);
+        }
+    }
+}
---
+++ src/MvcPlugins/File/Ornament.Files/Dao/IFileDaoFactory.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Qi.Domain;
+
+namespace Ornament.Files.Dao
+{
+    public interface IFileDao
+    : IDao<string, FileRecord>
+    {
+
+    }
+
+    public interface IFileDaoFactory
+    {
+        IFileDao CreateFileDao();
+    }
+}
