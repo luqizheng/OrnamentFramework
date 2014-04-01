fb71bb0357bea6a99776ce7e3e99bbcfaeba20e2
leo <luqizheng@126.com>
2014-04-01 11:01:23 +0800

    add lost file.

---
+++ src/MvcPlugins/Ornament.Files.Plugin/Areas/Files/Controllers/UploadFilesController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Ornament.Files.Plugin.Areas.Files.Controllers
+{
+    public class UploadFilesController : Controller
+    {
+        //
+        // GET: /Files/UploadFiles/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+        
+        public ActionResult Details()
+        {
+            return View();
+        }
+
+    }
+}
