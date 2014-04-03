1a35d69f2f5c1030716c19e531d87b1a8fb0a82e
leo <luqizheng@126.com>
2014-04-03 11:12:23 +0800

    add lost file.

---
+++ src/MvcPlugins/Ornament.Files.Plugin/Api/UploadFilesController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Web.Http;
+using System.Web.UI.WebControls;
+using Ornament.Files.Dao;
+using Qi.Web.Http;
+
+namespace Ornament.Files.Plugin.Api
+{
+    [ApiSession]
+    public class UploadFilesController : ApiController
+    {
+        private readonly IFileDaoFactory _fileDaoFactory;
+
+        public UploadFilesController(IFileDaoFactory fileDaoFactory)
+        {
+            _fileDaoFactory = fileDaoFactory;
+        }
+
+        //
+        // GET: /UploadFiles/
+        [HttpGet]
+        public System.Collections.Generic.IEnumerable<FileRecord> Index(FileRecordSearcher search)
+        {
+            int total;
+            return _fileDaoFactory.CreateFileDao().Find(search.Name, search.PageIndex.Value, search.PageSize.Value, out total);
+        }
+    }
+
+    public class FileRecordSearcher
+    {
+        private int? _pageIndex;
+        private int? _pageSize;
+        public string Name { get; set; }
+        public string SignCode { get; set; }
+
+        public int? PageSize
+        {
+            get
+            {
+                if (_pageSize == null)
+                    _pageSize = 40;
+                if (_pageSize > 100)
+                {
+                    _pageSize = 100;
+                }
+                return _pageSize;
+            }
+            set { _pageSize = value; }
+        }
+
+        public int? PageIndex
+        {
+            get { return _pageIndex ?? 0; }
+            set { _pageIndex = value; }
+        }
+    }
+}
