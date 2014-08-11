e0b19aedccbc9f80069d37a16b49f9c248d03dda
leo <leo@126.com>
2014-08-11 18:35:14 +0800

    ccc

---
+++ src/Mvc5WebApplication/App_Start/LauguageConfig.cs
@@ -0,0 +1,32 @@
+﻿using System.Web.ModelBinding;
+using Ornament.Contexts;
+using Ornament.MemberShip.Plugin.Models;
+using Ornament.MemberShip.Validations;
+using Ornament.Models;
+
+namespace WebApplication
+{
+    public static class LauguageConfig
+    {
+        public static void Register(OrnamentConfiguration context)
+        {
+            if (!context.Languages.Contains("en"))
+            {
+                context.Languages.Add(new Language("English", "en") { IsDefault = true });
+            }
+            if (!context.Languages.Contains("zh-Hant"))
+            {
+                context.Languages.Add(new Language("中文(繁)", "zh-Hant"));
+
+            }
+            if (!context.Languages.Contains("zh-Hans"))
+            {
+                context.Languages.Add(new Language("中文(简)", "zh-Hans"));
+            }
+
+          
+        }
+    }
+
+    
+}
---
+++ src/Mvc5WebApplication/App_Start/ValidationConfig.cs
@@ -0,0 +1,20 @@
+﻿using System.Web.Mvc;
+using Ornament.MemberShip.Plugin.Models;
+using Ornament.MemberShip.Validations;
+using Ornament.Validations;
+using Ornament.Web.ValidationAdapter;
+
+namespace WebApplication
+{
+    public class ValidationConfig
+    {
+        public void Registry()
+        {
+            DataAnnotationsModelValidatorProvider.RegisterAdapter(typeof(PasswordValidationAttribute), typeof(RegularExpressionAttributeAdapter));
+            DataAnnotationsModelValidatorProvider.RegisterAdapter(typeof(VerifyCodeRequireAttribute), typeof(RequiredAttributeAdapter));
+            //新的Attribute，用于JquerUI spinner控件一起用
+            DataAnnotationsModelValidatorProvider.RegisterAdapter(typeof(JqStepAttribute), typeof(StepAttributeAdapter));
+
+        }
+    }
+}
