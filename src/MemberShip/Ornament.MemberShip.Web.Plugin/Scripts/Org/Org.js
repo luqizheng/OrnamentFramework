c58845767a7424408ad1c068d8871f083d7f21b5
leo <leo@126.com>
2014-09-15 18:06:10 +0800

    oll

--- src/MemberShip/Ornament.MemberShip.Web.Plugin/Scripts/Org/Org.js
+++ src/MemberShip/Ornament.MemberShip.Web.Plugin/Scripts/Org/Org.js
@@ -1,43 +1,92 @@
 ﻿define(["text!./Templates/Org.html"], function (template) {
 
     var widget = avalon.ui.org = function (element, data, vmodels) {
-        
+
         var options = data.orgOptions,
           optionsTemplate = template;
 
+
+
         var vmodel = avalon.define(data.orgId, function (vm) {
-
+            vm.id = "";
             avalon.mix(vm, options);
-
-            vm.orgs = [{ Name: "", Childs: [] }];
+            vm.orgs = [{ Name: "",selected:false, Childs: [],Id:"" }];
             vm.selectOrg = null;
             vm.$init = function () {
-                var pageHTML = optionsTemplate;
+                var pageHTML = $(optionsTemplate);
+                pageHTML.find(".modal:first").attr("id", vm.id + "_modal");
                 element.style.display = "none";
-                element.innerHTML = pageHTML;
+                element.innerHTML += pageHTML.html();
+
                 setTimeout(function () {
-                    alert('ok');
+
                     avalon.scan(element, [vmodel].concat(vmodels));
+
                     element.style.display = "";
 
                     $.get("/Api/Memberships/Orgs", function (data) {
+                        function reset(aryOrgs) {
+                            for (var i = 0; i < aryOrgs.length; i++) {
+                                aryOrgs[i].selected = false;
+                                if (vm.curOrg && vm.curOrg.Id == data.Id) {
+                                    aryOrgs[i].selected = true;
+                                }
+                                if (aryOrgs[i].Childs && aryOrgs[i].Childs.length != 0) {
+                                    reset(aryOrgs[i].Childs);
+                                }
+                            }
+                            
+                        }
+
+                        reset(data);
                         vm.orgs = data;
                     });
-
                 }, 150);
-
-
             };
-            vm.$remove = function () { element.innerHTML = ""; };
-
+            vm.$remove = function () {
+                element.innerHTML = "";
+            };
+            vm.orgName = {
+                get: function() {
+                    return vm.curOrg ?
+                        vm.curOrg.Name : "请选择";
+                }
+            };
+            vm.orgId = {
+                get: function () {
+                    return vm.curOrg ?
+                            vm.curOrg.Name : "";
+                }
+            };
+            vm.curOrg = false;//已经选择了的node
             vm.toggle = function () {
+                
+                if (vm.curOrg) {
+                    vm.curOrg.selected = false;
+                }
+                vm.curOrg = this.$vmodel.el;
+                vm.curOrg.selected = true;
+                
+
+                var $childMenu = $(this).closest("li").find("ul:first");
+
                 if (this.$vmodel.el.Hide) {
-                    $(this).closest("li").find("ul:first").show('fast');
+                    $childMenu.show('fast');
                 } else {
-                    $(this).closest("li").find("ul:first").hide('fast');
+                    $childMenu.hide('fast');
                 }
                 this.$vmodel.el.Hide = !this.$vmodel.el.Hide;
             };
+
+            vm.ok = function () {
+                if (vm.curOrg) {
+                    options.OnOk(vm.curOrg);
+                }
+            };
+
+            vm.$skipArray = ["curName", "curId"];
+
+         
         });
 
 
@@ -46,7 +95,8 @@
 
     widget.defaults = {
         editable: false,
-        select: true
+        select: true,
+        OnOk: false
     };
     return avalon;
 });
