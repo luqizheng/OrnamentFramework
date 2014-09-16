8a4d590743d6f32e7fd286b2b430b1fdc7ec0188
leo <leo@126.com>
2014-09-16 18:04:50 +0800

    a

---
+++ src/Ornament.Web.UI/OrnamentValidation.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace Ornament.Web.UI
+{
+    public static class OrnamentValidation
+    {
+        public static MvcHtmlString OrnamentValidationSummary(this HtmlHelper helper)
+        {  
+            /*<div data-valmsg-summary="true" class="validation-summary-valid"><ul><li style="display:none"></li></ul></div>*/
+            string sb = @"<div class=""validation-summary-valid"" data-valmsg-summary=""true"" style=""display:none"">
+				<a href=""#"" data-dismiss=""alert"" class=""close"">×</a>
+				<h4 class=""alert-heading"">" + Properties.Resources.Wanring+"</h4></div>";
+
+            return new MvcHtmlString(sb);
+
+        }
+    }
+}
