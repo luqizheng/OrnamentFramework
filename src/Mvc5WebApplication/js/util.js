1b1211542a84d49034e89f2b683302297a6b1f02
leo <leo@126.com>
2014-09-23 16:32:46 +0800

    序列化的改搞到vaform里面

--- src/Mvc5WebApplication/js/util.js
+++ src/Mvc5WebApplication/js/util.js
@@ -1,16 +1,1 @@
 ﻿/* add by leo*/
-$.fn.serializeObject = function () {
-    var o = {};
-    var a = this.serializeArray();
-    $.each(a, function () {
-        if (o[this.name] !== undefined) {
-            if (!o[this.name].push) {
-                o[this.name] = [o[this.name]];
-            }
-            o[this.name].push(this.value || '');
-        } else {
-            o[this.name] = this.value || '';
-        }
-    });
-    return o;
-};
