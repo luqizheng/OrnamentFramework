4de5e12ac4d463a9197d0506f8060cce93dd2420
leo <leo@126.com>
2014-10-09 19:20:11 +0800

    123456

--- src/Chat/messageDao.js
+++ src/Chat/messageDao.js
@@ -1,3 +1,22 @@
 var  mongodb = require('mongodb');
 var  server  = new mongodb.Server('localhost', 27017, {auto_reconnect:true});
-var  db = new mongodb.Db('mydb', server, {safe:true});
+var  db = new mongodb.Db('ornamentMessages', server, {safe:true});
+exports.init=function(){
+	console.log('ok');
+}
+
+exports.send=function(msgObj){
+	/* {
+		to:"joe",
+		from:"hellen",
+		content:"xkxie",
+		createTime:"kkkdd",
+		read:true,
+		readTime:"createTime"
+	} */
+	
+}
+
+	
+
+
