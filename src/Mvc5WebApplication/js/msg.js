7388d538fe38fd4bcaaa5bc7b3c9982ecb88fc44
cadyfunny <luqizheng@126.com>
2014-11-09 17:10:13 +0800

    add msg.js for access chat system wirted by nodejs
    add msg.js for access chat system wirted by nodejs

---
+++ src/Mvc5WebApplication/js/msg.js
@@ -0,0 +1,10 @@
+﻿define(function(require) {
+    return function (host, userobj) {
+        var socket = require("socketio").connect(host);
+        socket.emit("reg user", userobj);
+
+        socket.on("reg user", function(data) {
+            console.log('reg user success='+data.success);
+        });
+    };
+})
