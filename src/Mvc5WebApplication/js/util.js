1151fd83b213e6ec3f599712aaf8e588392eb324
leo <leo@126.com>
2014-11-10 12:40:12 +0800

    upgrade to 1.5

--- src/Mvc5WebApplication/js/util.js
+++ src/Mvc5WebApplication/js/util.js
@@ -1,1 +1,7 @@
 ﻿/* add by leo*/
+$(document).ajaxError(function (event, jqxhr, settings, thrownError) {
+    if (jqxhr.status == 401) {
+        // perform a redirect to the login page since we're no longer authorized
+        window.location.replace("/Account/logon");
+    }
+});
