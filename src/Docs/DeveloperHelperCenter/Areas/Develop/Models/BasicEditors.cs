bdabdfe9fd9d1eb4cd3fc6c42d63acb6c6fef8b1
leo <leo@126.com>
2015-01-22 18:02:15 +0800

    加入develper的说明文档。以及代码显示

--- src/Docs/DeveloperHelperCenter/Areas/Develop/Models/BasicEditors.cs
+++ src/Docs/DeveloperHelperCenter/Areas/Develop/Models/BasicEditors.cs
@@ -9,20 +9,28 @@ namespace DeveloperHelperCenter.Areas.Develop.Models
         public BasicEditors()
         {
             Int32 = 6;
-            this.Int64 = 1800;
+            Int64 = 1800;
             Date = DateTime.Now;
         }
+
         [Range(5, 20)]
         public Int32 Int32 { get; set; }
+
         [QiRange(900D, 10000d, 100D)]
         public Int64 Int64 { get; set; }
+
         public string String { get; set; }
         public String Textarea { get; set; }
+
+        [QiRange(10D, 15d, 0.2D)]
         public Decimal Decimal { get; set; }
+        
+        [DataType(DataType.DateTime)]
         public DateTime DateTime { get; set; }
+
         [DataType(DataType.Date)]
         [DisplayFormat(DataFormatString = "yyyy/MM/dd")]
         [DateRange("1940-01-01", "2014-12-31", "yyyy-MM-dd", "yy/mm/dd")]
-        public DateTime Date { get; set; }
+        public DateTime? Date { get; set; }
     }
 }
