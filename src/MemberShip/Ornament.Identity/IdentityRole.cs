eb7ab5814b5e050716bb811b16ecd4a7d1cced9f
卢骐征 <cadylu@126.com>
2016-07-08 09:59:55 +0800

    修正一下版本号

--- src/MemberShip/Ornament.Identity/IdentityRole.cs
+++ src/MemberShip/Ornament.Identity/IdentityRole.cs
@@ -1,18 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
+using Ornament.Domain.Entities;
 
 namespace Ornament.Identity
 {
     /// <summary>
-    /// The default implementation of <see cref="IdentityRole{TKey}"/> which uses a string as the primary key.
+    ///     The default implementation of <see cref="IdentityRole{TKey}" /> which uses a string as the primary key.
     /// </summary>
     public class IdentityRole : IdentityRole<string>
     {
         /// <summary>
-        /// Initializes a new instance of <see cref="IdentityRole"/>.
+        ///     Initializes a new instance of <see cref="IdentityRole" />.
         /// </summary>
         /// <remarks>
-        /// The Id property is initialized to from a new GUID string value.
+        ///     The Id property is initialized to from a new GUID string value.
         /// </remarks>
         public IdentityRole()
         {
@@ -20,11 +21,11 @@ namespace Ornament.Identity
         }
 
         /// <summary>
-        /// Initializes a new instance of <see cref="IdentityRole"/>.
+        ///     Initializes a new instance of <see cref="IdentityRole" />.
         /// </summary>
         /// <param name="roleName">The role name.</param>
         /// <remarks>
-        /// The Id property is initialized to from a new GUID string value.
+        ///     The Id property is initialized to from a new GUID string value.
         /// </remarks>
         public IdentityRole(string roleName) : this()
         {
@@ -33,7 +34,7 @@ namespace Ornament.Identity
     }
 
     /// <summary>
-    /// Represents a role in the identity system
+    ///     Represents a role in the identity system
     /// </summary>
     /// <typeparam name="TKey">The type used for the primary key for the role.</typeparam>
     public class IdentityRole<TKey> : IdentityRole<TKey, IdentityRoleClaim<TKey>>
@@ -42,23 +43,25 @@ namespace Ornament.Identity
     }
 
     /// <summary>
-    /// Represents a role in the identity system
+    ///     Represents a role in the identity system
     /// </summary>
     /// <typeparam name="TKey">The type used for the primary key for the role.</typeparam>
     /// <typeparam name="TRoleClaim">The type used for role claims.</typeparam>
     public class IdentityRole<TKey, TRoleClaim>
-        : Ornament.Domain.Entities.EntityWithTypedId<TKey>
+        : EntityWithTypedId<TKey>
         where TKey : IEquatable<TKey>
         where TRoleClaim : IdentityRoleClaim<TKey>
 
     {
         /// <summary>
-        /// Initializes a new instance of <see cref="IdentityRole{TKey}"/>.
+        ///     Initializes a new instance of <see cref="IdentityRole{TKey}" />.
         /// </summary>
-        public IdentityRole() { }
+        public IdentityRole()
+        {
+        }
 
         /// <summary>
-        /// Initializes a new instance of <see cref="IdentityRole{TKey}"/>.
+        ///     Initializes a new instance of <see cref="IdentityRole{TKey}" />.
         /// </summary>
         /// <param name="roleName">The role name.</param>
         public IdentityRole(string roleName) : this()
@@ -68,32 +71,32 @@ namespace Ornament.Identity
 
 
         /// <summary>
-        /// Navigation property for claims in this role.
+        ///     Navigation property for claims in this role.
         /// </summary>
         public virtual ICollection<TRoleClaim> Claims { get; } = new List<TRoleClaim>();
 
         /// <summary>
-        /// Gets or sets the primary key for this role.
+        ///     Gets or sets the primary key for this role.
         /// </summary>
         public virtual TKey Id { get; set; }
 
         /// <summary>
-        /// Gets or sets the name for this role.
+        ///     Gets or sets the name for this role.
         /// </summary>
         public virtual string Name { get; set; }
 
         /// <summary>
-        /// Gets or sets the normalized name for this role.
+        ///     Gets or sets the normalized name for this role.
         /// </summary>
         public virtual string NormalizedName { get; set; }
 
         /// <summary>
-        /// A random value that should change whenever a role is persisted to the store
+        ///     A random value that should change whenever a role is persisted to the store
         /// </summary>
         public virtual string ConcurrencyStamp { get; set; } = Guid.NewGuid().ToString();
 
         /// <summary>
-        /// Returns the name of the role.
+        ///     Returns the name of the role.
         /// </summary>
         /// <returns>The name of the role.</returns>
         public override string ToString()
---
+++ src/WebApplication/Web/webpack.config.js
@@ -0,0 +1,66 @@
+﻿var fs = require("fs");
+var path = require('path');
+var glob = require('glob');
+var CommonsChunkPlugin = require("webpack/lib/optimize/CommonsChunkPlugin");
+
+
+var entryPages = getEntry('/src/pages/**/*', 'scripts\\\\src\\\\pages\\\\');
+
+module.exports = {
+    entry: entryPages,
+    devtool: "inline-source-map",
+    output: {
+        
+        filename: "[name].js",
+        path:"wwwroot/scripts"
+    },
+    resolve: {
+        extensions: ["", ".ts", ".js",".css",".less",".scss",".png",".jpg",".jpeg"],
+        alias: {
+            'jquery': __dirname + "/Scripts/jquery-2.2.2.min.js",
+            'avalon': __dirname + "/Scripts/avalon.mobile.shim.min.js",
+            'bootstrap': __dirname + "/Scripts/bootstrap.min.js",
+            'signalR': __dirname + "/Scripts/jquery.signalR.js",
+            'bootbox': __dirname + "/Scripts/bootbox.min.js",
+            'monent': __dirname + "/Scripts/moment.js"
+        }
+    },
+    module: {
+        loaders: [
+            { test: /\.ts$/, loader: 'ts-loader' }
+        ],
+        preLoaders: [
+          {
+              test: /\.ts$/,
+              loader: "tslint-loader"
+          }
+        ]
+    },
+    plugins: [new CommonsChunkPlugin("common.js")],
+    externals: {
+        'jquery': 'jQuery',
+        '$': 'jquery',
+        'avalon': 'avalon'
+
+    }
+};
+
+function getEntry(globPath, pathDir) {
+    var files = glob.sync(globPath);
+    var entries = {},
+        entry, dirname, basename, pathname, extname;
+
+    for (var i = 0; i < files.length; i++) {
+        entry = files[i];
+        dirname = path.dirname(entry);
+        extname = path.extname(entry);
+        basename = path.basename(entry, extname);
+        pathname = path.join(dirname, basename);
+        pathname = pathDir ? pathname.replace(new RegExp('^' + pathDir), '') : pathname;
+
+        entries[pathname] = './' + entry;
+
+    }
+    console.log(entries)
+    return entries;
+}
